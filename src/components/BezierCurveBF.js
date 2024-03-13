eadbc00a81762f993676ad2a4d8e72d3c8b51419
AtqiyaHaydar <ashford1524@gmail.com>
2024-03-13 22:20:28 +0700

    feat: add home

---
+++ src/components/BezierCurveBF.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BezierCurveBF = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BezierCurveBF
---
+++ src/components/BezierCurveDnC.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BezierCurveDnC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default BezierCurveDnC
