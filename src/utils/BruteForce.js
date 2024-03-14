2f5a12d2c510304adf9abd236583e6ae6a0bc641
fnathas <farrel.saskoro@gmail.com>
2024-03-14 23:59:02 +0700

    refactor: add function for beizer curve in brute force

--- src/utils/BruteForce.js
+++ src/utils/BruteForce.js
@@ -1,6 +1,20 @@
 
-function BruteForce(points, iterations) {
-  console.log(" Start ")
+// Functions to generate points on beizer curve with the iteration of the curve
+function quadraticBezierFormula(points) {
+  const x = Math.pow(1 - t, 2) * points[0].x + 2 * (1 - t) * t * points[i].x + Math.pow(t, 2) * points[2].x;
+  const y = Math.pow(1 - t, 2) * points[0].y + 2 * (1 - t) * t * points[i].y + Math.pow(t, 2) * points[2].y;
+  return { x, y };
+}
 
-  
+function quadraticBezierGeneratorBruteForce(points, iterations) {
+  let result = [];
+
+  console.time('Brute Force');
+  for (let i = 0; i <= iterations; i++) {
+      const t = i / iterations;
+      const point = quadraticBezierBruteForce(points, t);
+      result.push(point);
+  }
+  console.timeEnd('Brute Force');
+  return result;
 }
