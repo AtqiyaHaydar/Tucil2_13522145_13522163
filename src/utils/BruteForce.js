8840d74fef91aa41f8b57e4d6660b9676396d2c7
fnathas <farrel.saskoro@gmail.com>
2024-03-16 15:35:36 +0700

    feat: add beizer curve brute force function with n-points input

--- src/utils/BruteForce.js
+++ src/utils/BruteForce.js
@@ -13,8 +13,32 @@ function pascalTriangle(n) {
   return result[n - 1];
 }
 
+// Functions to calculate Bezier curve using brute force with n-points
+function BezierFormula(points, n, t) {
+  const pascal = pascalTriangle(n);
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < n; i++) {
+    x += Math.pow(1 - t, n  - 1 - i) * Math.pow(t, i) * pascal[i] * points[i].x;
+    y += Math.pow(1 - t, n - 1 - i) * Math.pow(t, i) * pascal[i] * points[i].y;
+  }
+  return { x, y };
+}
 
-// Functions to generate points on beizer curve with the iteration of the curve
+export function BezierGeneratorBruteForce(points, n, iterations) {
+  let result = [];
+
+  for (let i = 0; i <= iterations; i++) {
+      const t = i / iterations;
+      const point = BezierFormula(points, n, t);
+      result.push(point);
+  }
+
+  return result;
+}
+
+
+// Functions to calculate Bezier curve using brute force with only 3-points
 function quadraticBezierFormula(points, t) {
   const x = Math.pow(1 - t, 2) * points[0].x + 2 * (1 - t) * t * points[1].x + Math.pow(t, 2) * points[2].x;
   const y = Math.pow(1 - t, 2) * points[0].y + 2 * (1 - t) * t * points[1].y + Math.pow(t, 2) * points[2].y;
